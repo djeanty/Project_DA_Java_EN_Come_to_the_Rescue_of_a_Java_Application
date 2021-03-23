6cf4f67c885ae5f0b8cb6ee13e0778b72041a04b
RedCoder42 <damien.jeanty@gmail.com>
2021-03-23 09:39:33 +0100

    Handling the case where results.out already exists before using the software and generation of javadoc.

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomCount.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomCount.java
@@ -1,5 +1,6 @@
 package com.hemebiotech.analytics;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,22 +12,21 @@ import java.util.TreeMap;
  * Creates a TreeMap that is made of the symptom name (String) and the amount of times it has been seen (Integer).
  * @author Damien Jeanty
  *
+ *
  */
 public class SymptomCount {
 	
-	private Map<String, Integer> symptomsMap = new TreeMap<String, Integer>();
-	
-	/* TODO: Possibility to upgrade the code :
-	 * If the output file already exists, read it and use it as a base for the map.
-	*/
-	
-	// TODO: @bug if there are multiple line breaks in the symptoms.txt, it will be counted as one 
+	private Map<String, Integer> symptomsMap = new TreeMap<String, Integer>(); 
 	
 	/**
 	 * Creates the map from a file of symptoms.
 	 * @param filePath The file path to read the symptoms from. If the file at the given path has not been found, prints a message to the standard output.
 	 */
 	public SymptomCount(String filePath) {
+		File output = new File("/home/dj/git/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/results.out");
+		if (output.exists()) {
+			useExistingOutputFile();
+		}
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(filePath));
 			String line = reader.readLine();
@@ -46,6 +46,37 @@ public class SymptomCount {
 			reader.close();
 		} catch (FileNotFoundException fnf) {
 			System.out.println("Symptom file not found : "+filePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * If there is an existing results.out file, uses it to implements symptomsMap.
+	 * 
+	 */
+	public void useExistingOutputFile(){
+		try {
+			
+			BufferedReader reader = new BufferedReader(new FileReader("/home/dj/git/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/results.out"));
+			String line = reader.readLine();
+			
+			String[] myLine = new String[2];
+			
+			while (line != null) {
+				if (!line.equals("")) {
+					
+					myLine = line.split(" : ");
+					
+					if (symptomsMap.containsKey(myLine[0])) {
+						symptomsMap.replace(myLine[0], (Integer.parseInt(myLine[1])+1));
+					} else {
+						symptomsMap.put(myLine[0], Integer.parseInt(myLine[1]));
+					}
+				}
+				line = reader.readLine();
+			}
+			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
